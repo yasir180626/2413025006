3e955cbd6d89ae6dee37eaa52abf8e7c2e60bd2d
2413025006 <muhamadyasir180626@gmail.com>
2025-03-28 22:32:13 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+(function($) {
+    'use strict';
+	
+	jQuery(document).ready(function(){
+		
+		
+		/* START MOBILE-MENU  */
+			$('.main_menu').slicknav({
+				prependTo:".mobile-nav",
+			});
+		/* START MOBILE-MENU  */
+		
+	});	
+	
+		/*PRELOADER JS*/
+			$(window).on('load', function() {  
+				$('.spinner').fadeOut();
+				$('.preloader').delay(350).fadeOut('slow'); 
+			}); 
+		/*END PRELOADER JS*/
+		
+		// Wow
+			new WOW().init();
+})(jQuery);
